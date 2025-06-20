4ff72ee7ec96902255f28e7f89382af492976e99
DevinaRatasyaSalsabila <devinaratasya2812@gmail.com>
2025-06-20 11:08:57 +0700

    /

--- app/Http/Controllers/NotulenController.php
+++ app/Http/Controllers/NotulenController.php
@@ -7,22 +7,42 @@ use Illuminate\Http\Request;
 class NotulenController extends Controller
 {
     public function store(Request $request)
+        {
+            $lastUser = Notulen::max('id_user') ?? 0;
+            $lastBagian = Notulen::max('id_bagian') ?? 0;
+            $lastGuru = Notulen::max('id_guru') ?? 0;
+
+            $notulen = new Notulen();
+            $notulen->id_user = $lastUser + 1;
+            $notulen->id_bagian = $lastBagian + 1;
+            $notulen->id_guru = $lastGuru + 1;
+
+            $notulen->tgl = $request->tgl;
+            $notulen->tahunajaran = $request->tahunajaran;
+            $notulen->agenda = $request->agenda;
+            $notulen->kesimpulan = $request->kesimpulan;
+            $notulen->status = $request->status;
+            $notulen->tempat = $request->tempat;
+            $notulen->jenis_undangan = $request->jenis_undangan;
+
+            $notulen->save();
+
+            return redirect()->route('notulen.index')->with('success', 'Data notulen berhasil disimpan.');
+        }
+
+        public function storePeserta(Request $request)
     {
         $request->validate([
-            'id_user'        => 'required|integer',
-            'id_bagian'      => 'required|integer',
-            'tgl'            => 'required|date',
-            'tahunajaran'    => 'required|string',
-            'agenda'         => 'nullable|string',
-            'kesimpulan'     => 'nullable|string',
-            'id_guru'        => 'nullable|integer',
-            'status'         => 'required|in:pending,approved,rejected',
-            'tempat'         => 'nullable|string',
-            'jenis_undangan' => 'required|in:umum,guru,siswa',
+            'id_notulen'     => 'required|integer',
+            'nama_peserta'   => 'required|string',
+            'jabatan'        => 'nullable|string',
+            'jabatan_rapat'  => 'nullable|string',
+            'ttd'            => 'nullable|string',
+            'urutan'         => 'nullable|string|max:2',
         ]);
 
-        Notulen::create($request->all());
+        PesertaNotulen::create($request->all());
 
-        return redirect()->route('notulen.index')->with('success', 'Notulen berhasil dibuat.');
+        return redirect()->route('peserta_notulen.index')->with('success', 'Peserta notulen berhasil ditambahkan.');
     }
 }
